ctcit/triphub

commit 3d459a5b1d08b50cdb2e7fb1905e01c9bae7f9c4

#39 #42 Changed name control to a select and a conditional text Added support for option groups to SelectControl Renamed id to field in control classes Changed some methods to properties Removed a bunch of unnecessary method bindings Further simplfied App.render()

src/TripParticipants.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
 import { Button, Navbar, ButtonGroup, ListGroup, ListGroupItem } from 'reactstrap';
 import { Component } from 'react';
-import { IMember, IParticipant } from './Interfaces';
+import { IParticipant } from './Interfaces';
 import { Spinner } from './Widgets';
 import { Trip } from './Trip';
 import { App } from './App';
 import { GetDisplayPriority } from './Utilities';
 import { TripParticipant } from './TripParticipant';
-import { ButtonWithTooltip } from './MapEditor';
 
 export class TripParticipants extends Component<{
     trip: Trip,
     app: App,
     isLoading: boolean
 }, {
-    namePrefix?: string
 }> {
 
     public participant: React.RefObject<any>
@@ -36,12 +34,12 @@ export class TripParticipants extends Component<{
     public signUpTramper() {
         const participants = this.props.trip.state.participants
 
-        this.props.trip.setState({ participants: [this.props.trip.blankTramper(), ...participants] })
+        this.props.trip.setState({ participants: [...participants, this.props.trip.blankTramper] })
     }
 
     public signMeUp() {
 
-        if (!this.props.app.getMe().id) {
+        if (!this.props.app.me.id) {
             const newMembersRep = this.props.app.state.members[0]
             alert(`Non members are most welcome to join club trips.\n` +
                 `Please either:\n` +
@@ -53,7 +51,8 @@ export class TripParticipants extends Component<{
         }
 
         this.props.trip.setState({ isSaving: true })
-        this.props.app.triphubApiCall('POST', this.props.trip.props.href + '/participants', this.props.trip.signMeUpTramper(), true)
+        this.props.app.triphubApiCall('POST', this.props.trip.props.href + '/participants',
+            this.props.trip.signMeUpTramper, true)
             .then(this.props.trip.requeryParticipants)
     }
 
@@ -66,7 +65,7 @@ export class TripParticipants extends Component<{
     }
 
     public signUpTramperCancel() {
-        this.props.trip.setState({ participants: this.props.trip.state.participants.filter((p: any) => p.id !== -1) })
+        this.props.trip.setState({ participants: this.props.trip.state.participants.filter(p => p.id !== -1) })
     }
 
     public toggleLegend() {
@@ -74,7 +73,7 @@ export class TripParticipants extends Component<{
     }
 
     public setPosition(id: number, target?: IParticipant, showMenu?: boolean): Promise<any> {
-        const info = this.props.trip.getParticipantsInfo()
+        const info = this.props.trip.participantsInfo
         const source = info.all.find(p => p.id === id) as IParticipant
         const sourceIndex = info.moveable.indexOf(source)
         let displayPriority = source.id
@@ -122,10 +121,10 @@ export class TripParticipants extends Component<{
 
     public render() {
 
-        const me = this.props.app.getMe()
+        const me = this.props.app.me
         const anon = !me.id
-        const info = this.props.trip.getParticipantsInfo()
-        const isPrivileged = this.props.trip.canEditTrip()
+        const info = this.props.trip.participantsInfo
+        const isPrivileged = this.props.trip.canEditTrip
         const isOpen = this.props.trip.state.trip.isOpen || isPrivileged
         const isNewTrip = this.props.trip.props.isNew
         const hasNewTramper = !!info.all.find((p: IParticipant) => p.id === -1)
@@ -134,23 +133,26 @@ export class TripParticipants extends Component<{
         const onDropOnWaitlist = (ev: any) => this.setPosition(parseInt(ev.dataTransfer.getData('id'), 10), undefined)
         const showLegend = this.props.trip.state.showLegend
         const legendIcon = (className: string, description: string) =>
-            <div><span className={className}/>{description}</div>
+            <div><span className={className} />{description}</div>
         const legendButton = (className: string, description: string) =>
-            <div><Button disabled={true}><span className={className}/></Button>{description}</div>
+            <div><Button disabled={true}><span className={className} /></Button>{description}</div>
 
         return [
             <Navbar key='navbar' color='light' light={true} expand='md'>
                 {[
-                    <Button key={'signmeup' + info.all.length} onClick={this.signMeUp} hidden={this.props.isLoading || isNewTrip || imOnList || !isOpen}>
+                    <Button key={'signmeup' + info.all.length} onClick={this.signMeUp}
+                        hidden={this.props.isLoading || isNewTrip || imOnList || !isOpen}>
                         <span className='fa fa-pen wiggle' />
                         {this.props.trip.state.isSaving ? ['Signing up ', Spinner] : 'Sign me up!'}
                         {info.current.length >= info.maxParticipants ? " (on waitlist)" : ""}
                     </Button>,
-                    <Button key={'signup' + info.all.length} onClick={this.signUpTramper} hidden={this.props.isLoading || isNewTrip || hasNewTramper || !isOpen || anon || !isPrivileged}>
+                    <Button key={'signup' + info.all.length} onClick={this.signUpTramper}
+                        hidden={this.props.isLoading || isNewTrip || hasNewTramper || !isOpen || anon || !isPrivileged}>
                         <span className='fa fa-user-plus' /> Sign up a tramper
                     {info.current.length >= info.maxParticipants ? " (on waitlist)" : ""}
                     </Button>,
-                    <ButtonGroup key={'signupcomplete' + info.all.length} hidden={this.props.isLoading || isNewTrip || !hasNewTramper || !isOpen}>
+                    <ButtonGroup key={'signupcomplete' + info.all.length}
+                        hidden={this.props.isLoading || isNewTrip || !hasNewTramper || !isOpen}>
                         <Button onClick={this.signUpTramper} disabled={true}>
                             <span className='fa fa-user-plus' /> Sign up a tramper:
                     </Button>
@@ -170,51 +172,49 @@ export class TripParticipants extends Component<{
                 ]}
             </Navbar>,
             (showLegend ? <div className='participant-buttons-legend'>
-                {legendIcon('fas fa-star','This person is the leader')}
-                {legendIcon('fas fa-podcast','This person is taking a Personal Location Beacon')}
-                {legendIcon('fas fa-car','This person is taking a Car')}
-                {legendIcon('fas fa-comment','There is special logistical information here')}
-                {legendIcon('fas fa-id-badge','This person is not a member of the CTC')}
-                {legendButton('fa fa-angle-up','Moves the person up the list')}
-                {legendButton('fa fa-angle-down','Moves the person down the list')}
-                {legendButton('fa fa-trash','Takes the person off the list')}
-                {legendButton('fa fa-sm fa-pen','Puts the person back on the list')}
-                {legendButton('fa fa-sm fa-user-plus','Puts the person on the wait-list')}
-                {legendButton('fa fa-sm fa-user-times','Takes the person off the wait-list')}
-                {legendButton('fa fa-sm fa-phone','Updates emergency contact details')}
+                {legendIcon('fas fa-star', 'This person is the leader')}
+                {legendIcon('fas fa-podcast', 'This person is taking a Personal Location Beacon')}
+                {legendIcon('fas fa-car', 'This person is taking a Car')}
+                {legendIcon('fas fa-comment', 'There is special logistical information here')}
+                {legendIcon('fas fa-id-badge', 'This person is not a member of the CTC')}
+                {legendButton('fa fa-angle-up', 'Moves the person up the list')}
+                {legendButton('fa fa-angle-down', 'Moves the person down the list')}
+                {legendButton('fa fa-trash', 'Takes the person off the list')}
+                {legendButton('fa fa-sm fa-pen', 'Puts the person back on the list')}
+                {legendButton('fa fa-sm fa-user-plus', 'Puts the person on the wait-list')}
+                {legendButton('fa fa-sm fa-user-times', 'Takes the person off the wait-list')}
+                {legendButton('fa fa-sm fa-phone', 'Updates emergency contact details')}
             </div> : null),
             <ListGroup key='participants'>
                 <ListGroupItem>
                     {
                         info.current.map(p =>
-                            <TripParticipant key={`${p.id}${p.displayPriority}${p.isDeleted}`} participant={p} trip={this.props.trip}
-                                owner={this} app={this.props.app} loading={this.props.isLoading} canWaitList={info.late.length !== 0} ref={this.participant}
-                                info={info} />)
+                            <TripParticipant key={`${p.id}${p.displayPriority}${p.isDeleted}`} participantId={p.id}
+                                data={JSON.stringify(p)} trip={this.props.trip}
+                                owner={this} app={this.props.app} loading={this.props.isLoading}
+                                canWaitList={info.late.length !== 0} ref={this.participant} info={info} />)
                     }
                 </ListGroupItem>
                 <ListGroupItem hidden={info.late.length === 0}>
                     <div onDragOver={onDragOver} onDrop={onDropOnWaitlist}><b>Waitlist</b></div>
                     {
                         info.late.map(p =>
-                            <TripParticipant key={`${p.id}${p.displayPriority}${p.isDeleted}`} participant={p} trip={this.props.trip}
-                                owner={this} app={this.props.app} loading={this.props.isLoading} canUnwaitList={true} info={info} />)
+                            <TripParticipant key={`${p.id}${p.displayPriority}${p.isDeleted}`} participantId={p.id}
+                                data={JSON.stringify(p)} trip={this.props.trip}
+                                owner={this} app={this.props.app} loading={this.props.isLoading}
+                                canUnwaitList={true} info={info} />)
                     }
                 </ListGroupItem>
                 <ListGroupItem hidden={isNewTrip || info.deleted.length === 0}>
                     <div onDragOver={onDragOver} onDrop={this.onDropOnDeleted}><b>Deleted</b></div>
                     {
                         info.deleted.map(p =>
-                            <TripParticipant key={`${p.id}${p.displayPriority}${p.isDeleted}`} participant={p} trip={this.props.trip}
+                            <TripParticipant key={`${p.id}${p.displayPriority}${p.isDeleted}`} participantId={p.id}
+                                data={JSON.stringify(p)} trip={this.props.trip}
                                 owner={this} app={this.props.app} loading={this.props.isLoading} info={info} />)
                     }
                 </ListGroupItem>
             </ListGroup>,
-            <datalist key='memberlist' id='memberlist'>
-                {this.props.app.getMembers()
-                    .filter((m: IMember) => this.state.namePrefix && m.name.toUpperCase().startsWith(this.state.namePrefix.toUpperCase()))
-                    .filter((m: IMember, i: number) => i < 10)
-                    .map((m: any) => <option key={'datalistoption' + m.id} value={m.name} />)}
-            </datalist>
         ]
     }
 }

src/App.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Component } from 'react';
 import * as React from 'react';
-import { BaseOpt, BaseUrl } from '.';
+import { BaseUrl } from '.';
 import { IMember, IConfig, IMap, ITrip, IValidation, IParticipant, IHoliday, IArchivedRoute, Role } from './Interfaces';
 import { Trip } from './Trip';
 import { TripsList } from './TripsList';
@@ -26,8 +26,10 @@ export class App extends Component<{
     isLoadingMembers: boolean
     isLoadingHolidays: boolean
     role: Role
+    me: IMember
     members: IMember[]
     membersById: { [id: number]: IMember }
+    membersByName: { [name: string]: IMember }
     maps: IMap[],
     archivedRoutes: IArchivedRoute[],
     holidayMap: { [id: string]: IHoliday }
@@ -59,7 +61,9 @@ export class App extends Component<{
             isLoadingHolidays: true,
             role: Role.NonMember,
             members: [],
+            me: {} as IMember,
             membersById: {},
+            membersByName: {},
             maps: [],
             archivedRoutes: [],
             holidayMap: {},
@@ -68,17 +72,10 @@ export class App extends Component<{
             inIFrame: true, // true if to style/behave for use in iFrame; false to style/behave as standalone app
             notifications: []
         }
-        this.setStatus = this.setStatus.bind(this)
-        this.triphubApiCall = this.triphubApiCall.bind(this)
-        this.getMembers = this.getMembers.bind(this)
-        this.getMe = this.getMe.bind(this)
-        this.getMaps = this.getMaps.bind(this)
-        this.changePath = this.changePath.bind(this)
-        this.handlePopState = this.handlePopState.bind(this)
         this.trip = React.createRef()
         this.triplist = React.createRef()
         this.calendar = React.createRef()
-        window.top.onpopstate = this.handlePopState
+        window.top.onpopstate = this.handlePopState.bind(this)
     }
 
     public handlePopState(event: PopStateEvent) {
@@ -106,9 +103,9 @@ export class App extends Component<{
     }
 
 
-    public amAdmin(): boolean { return this.state.role >= Role.Admin }
+    public get amAdmin(): boolean { return this.state.role >= Role.Admin }
 
-    public amTripLeader(): boolean { return this.state.role >= Role.TripLeader }
+    public get amTripLeader(): boolean { return this.state.role >= Role.TripLeader }
 
     public async triphubApiCall(method: string, url: string, data?: any, isTripEdit?: boolean): Promise<any> {
         if (isTripEdit && this.trip.current && this.trip.current.state.editHref && !this.trip.current.state.editIsEdited) {
@@ -117,48 +114,57 @@ export class App extends Component<{
         return apiCall(method, url, data)
     }
 
-    public getMembers(): IMember[] {
+    public get members(): IMember[] {
         return this.state.members
     }
 
-    public getMe(): IMember {
-        return this.state.members.find((m: IMember) => m.isMe) || {} as unknown as IMember
+    public get me(): IMember {
+        return this.state.me
     }
 
     public getMemberById(id: number): IMember {
         return this.state.membersById[id] || { id, name: 'Anonymous' } as IMember
     }
 
-    public getMaps(): IMap[] {
+    public getMemberByName(name: string): IMember | null {
+        return this.state.membersByName[name]
+    }
+
+    public get maps(): IMap[] {
         return this.state.maps
     }
 
-    public getArchivedRoutes(): IArchivedRoute[] {
+    public get archivedRoutes(): IArchivedRoute[] {
         return this.state.archivedRoutes;
     }
 
     public validateTrip(trip: ITrip): IValidation[] {
         return !this.state.isLoading ? [
-            { id: 'title', ok: !trip.isDeleted, message: 'This trip has been deleted' },
+            { field: 'title', ok: !trip.isDeleted, message: 'This trip has been deleted' },
             ...this.mandatory(trip, ['title', 'description']),
             ...this.mandatory(trip, trip.isSocial ? [] : ['cost', 'grade', 'departure_point']),
-            { id: 'length', ok: (trip.length >= 1 && trip.length <= 14) || trip.isNoSignup, message: 'Length should be between 1 and 14 days' },
-            { id: 'openDate', ok: (trip.openDate <= trip.closeDate), message: 'Open date must be on or before Close date' },
-            { id: 'closeDate', ok: (trip.openDate <= trip.closeDate) || trip.isNoSignup, message: 'Open date must be on or before Close date' },
-            { id: 'closeDate', ok: (trip.closeDate <= trip.tripDate) || trip.isNoSignup, message: 'Close date must be on or before Trip date' },
-            { id: 'tripDate', ok: (trip.closeDate <= trip.tripDate) || trip.isNoSignup, message: 'Close date must be on or before Trip date' },
-            { id: 'maxParticipants', ok: trip.maxParticipants >= 0, message: 'Max Participants must not be negative' },
+            { field: 'length', ok: (trip.length >= 1 && trip.length <= 14) || trip.isNoSignup, message: 'Length should be between 1 and 14 days' },
+            { field: 'openDate', ok: (trip.openDate <= trip.closeDate), message: 'Open date must be on or before Close date' },
+            { field: 'closeDate', ok: (trip.openDate <= trip.closeDate) || trip.isNoSignup, message: 'Open date must be on or before Close date' },
+            { field: 'closeDate', ok: (trip.closeDate <= trip.tripDate) || trip.isNoSignup, message: 'Close date must be on or before Trip date' },
+            { field: 'tripDate', ok: (trip.closeDate <= trip.tripDate) || trip.isNoSignup, message: 'Close date must be on or before Trip date' },
+            { field: 'maxParticipants', ok: trip.maxParticipants >= 0, message: 'Max Participants must not be negative' },
         ] : []
     }
 
     public validateParticipant(participant: IParticipant): IValidation[] {
         return [
-            { id: 'vehicleRego', ok: !participant.isVehicleProvider || participant.vehicleRego !== '', message: 'Rego must be specified' }
+            {
+                field: 'vehicleRego', ok: !participant.isVehicleProvider || participant.vehicleRego !== '',
+                message: 'Rego must be specified'
+            }
         ]
     }
 
-    public mandatory(obj: any, ids: string[]): IValidation[] {
-        return ids.map(id => ({ id, ok: obj[id] !== '', message: TitleFromId(id) + ' must be entered' }))
+    public mandatory(obj: any, fields: string[]): IValidation[] {
+        return fields.map(field => ({
+            field, ok: obj[field] !== '', message: TitleFromId(field) + ' must be entered'
+        }))
     }
 
     public requeryMembers(): void {
@@ -166,18 +172,17 @@ export class App extends Component<{
             .then((members: IMember[]) => {
 
                 const membersById: { [id: number]: IMember } = {}
-                let myRole = Role.NonMember
-                for (const member of members) {
-                    member.role = Role[member.role as unknown as string]
-                    if (member.id) {
-                        membersById[member.id] = member
-                        if (member.isMe) {
-                            myRole = member.role
-                        }
-                    }
-                }
+                const membersByName: { [name: string]: IMember } = {}
+                const me = members.find((m: IMember) => m.isMe) || {} as unknown as IMember
 
-                this.setState({ role: myRole, members, membersById, isLoadingMembers: false })
+                members = members.filter(m => m.name !== '')
+                members.forEach(m => m.role = Role[m.role as unknown as string])
+                members.filter(m => m.isMember).forEach(m => membersById[m.id] = m)
+                members.filter(m => m.isMember).forEach(m => membersByName[m.name] = m)
+                members = members.filter(m => m.isMember || !membersByName[m.name])
+                members.filter(m => !m.isMember).forEach(m => membersByName[m.name] = m)
+
+                this.setState({ role: me.role, me, members, membersById, membersByName, isLoadingMembers: false })
             })
     }
 
@@ -214,48 +219,30 @@ export class App extends Component<{
     }
 
     public render() {
-
-        let content: JSX.Element
         const state = this.state
         const loading = Object.keys(state).filter(k => /isLoading./.test(k))
-
-        if (loading.some(name => state[name])) {
-            content =
-                <Container
-                    className={this.containerClassName() + "triphub-loading-container"}>
+        const rendering = loading.some(name => state[name]) ? 'loading' : `${state.path}/`.split('/')[1]
+        const renderings = {
+            loading: () =>
+                <Container className={this.containerClassName() + "triphub-loading-container"}>
                     <Jumbotron key='loadingAlert' variant='primary'>
                         {loading.map(name =>
                             <div key={name}>{state[name] ? Spinner : Done} {TitleFromId(name.substr(2))}</div>)}
                     </Jumbotron>
-                </Container>
-        } else {
-            switch (`${state.path}/`.split('/')[1]) {
-                case 'calendar':
-                    content = <Calendar app={this} />
-                    break
-                case 'newtrip':
-                    content = <Trip app={this} isNew={true} isNewSocial={false} />
-                    break
-                case 'newsocial':
-                    content = <Trip app={this} isNew={true} isNewSocial={true} />
-                    break
-                case 'newsletter':
-                    content = <Newsletter app={this} />
-                    break
-                case 'trips':
-                    content = <Trip app={this} isNew={false} isNewSocial={true} href={BaseUrl + state.path} />
-                    break
-                default:
-                    content = <TripsList app={this} />
-                    break
-            }
+                </Container>,
+            calendar: () => <Calendar app={this} />,
+            newtrip: () => <Trip app={this} isNew={true} isNewSocial={false} />,
+            newsocial: () => <Trip app={this} isNew={true} isNewSocial={true} />,
+            newsletter: () => <Newsletter app={this} />,
+            trips: () => <Trip app={this} isNew={false} isNewSocial={true} href={BaseUrl + state.path} />,
+            default: () => <TripsList app={this} />,
         }
 
         return [
             <TriphubNavbar key='triphubNavbar' app={this} />,
             <NotificationArea notifications={state.notifications} key='notificationArea'
                 containerClassName={this.containerClassName()} />,
-            content
+            (renderings[rendering] || renderings.default)()
         ]
     }
 

src/Calendar.tsx
@@ -66,9 +66,9 @@ class CalendarWeek extends Component<{
                     const oldTrip = item.trip as ITrip
                     const newTrip = { ...oldTrip, [field]: GetDateString(date) } as ITrip
                     const before = this.props.calendar.props.app.validateTrip(oldTrip)
-                                            .find(i => i.id === field && !i.ok)
+                                            .find(i => i.field === field && !i.ok)
                     const after = this.props.calendar.props.app.validateTrip(newTrip)
-                                            .find(i => i.id === field && !i.ok)
+                                            .find(i => i.field === field && !i.ok)
 
                     if (!before && after &&
                         !window.confirm('Trip validation resulted in this message:\n\n' + after.message + '\n\nproceed anyway?')) {
@@ -169,8 +169,6 @@ export class Calendar extends Component<{
             state: StateFilter.Open,
             selected: 0, trips: [],
         }
-        this.processTrips = this.processTrips.bind(this)
-        this.requery = this.requery.bind(this)
         this.showItem = this.showItem.bind(this)
     }
 

commit 31e9d8d3afaa2f749ea0db3992c79e6d4e05077f

Fix a bug introduced in 3d459a5 that broke Sign up p a tramper

src/TripParticipants.tsx
@@ -49,7 +49,7 @@ export class TripParticipants extends Component<{
     }
 
     public onSignUpTramperSave() {
-        const participant = this.props.trip.state.participants[0]
+        const participant = this.props.trip.state.participants[this.props.trip.state.participants.length - 1]
 
         this.props.trip.setState({ isSaving: true })
         this.props.app.triphubApiCall('POST', this.props.trip.props.href + '/participants', participant, true)
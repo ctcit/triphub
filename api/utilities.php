16209662648c1015a2380d641af6ff265ed8bc44
Nick Edwards <nickedwrds@gmail.com>
2022-06-28 22:00:11 +1200

    Update utilities.php for Joomla4

--- api/utilities.php
+++ api/utilities.php
@@ -4,7 +4,26 @@ define('_JEXEC', 1);
 define('JPATH_BASE', dirname(dirname(__DIR__)));// Assume we are two levels down in website
 require_once ( JPATH_BASE.'/includes/defines.php' );
 require_once ( JPATH_BASE.'/includes/framework.php' );
-$app = JFactory::getApplication('site');
+
+// Boot the DI container
+$container = \Joomla\CMS\Factory::getContainer();
+
+/*
+* Alias the session service keys to the web session service as that is the primary session backend for this application
+*
+* In addition to aliasing "common" service keys, we also create aliases for the PHP classes to ensure autowiring objects
+* is supported.  This includes aliases for aliased class names, and the keys for aliased class names should be considered
+* deprecated to be removed when the class name alias is removed as well.
+*/
+$container->alias('session.web', 'session.web.site')
+    ->alias('session', 'session.web.site')
+    ->alias('JSession', 'session.web.site')
+    ->alias(\Joomla\CMS\Session\Session::class, 'session.web.site')
+    ->alias(\Joomla\Session\Session::class, 'session.web.site')
+    ->alias(\Joomla\Session\SessionInterface::class, 'session.web.site');
+
+// Instantiate the application.
+$app = $container->get(\Joomla\CMS\Application\SiteApplication::class);
 $user = JFactory::getUser();
 $config = JFactory::getConfig();
 
